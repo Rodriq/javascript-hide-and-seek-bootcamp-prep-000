deccaf58e43dbdbeb938a943dbaf095bd8b14f74
Fogang Rodriq <fogangrodriq@gmail.com>
2018-07-24 23:50:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 
+function getFirstSelector(selector){
+  var vv = document.querySelector(selector);
+  return vv;
+}
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+function increaseRankBy(n){
+  n = parseInt(n)
+ var pos =  document.querySelector('ul.ranked-list li')
+ for(var i = 0; i<pos.length; i++){
+   pos[i].innerHTML = (i+n);
+ }
+}
+function deepestChild(){
+  return document.querySelector('#grand-node div div div div')
+}
